81d281f346fc8eb07b852e3d4bd74c09926d385b
albrechetti <luan@albrechetti.com>
2022-01-17 09:23:48 -0300

    isolate

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_playground/isolate/isolate_counter.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: CounterApp(),
-  ));
+  runApp(
+    MaterialApp(
+      themeMode: ThemeMode.dark,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const CounterApp(),
+    ),
+  );
 }
